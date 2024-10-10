0a309c77c194ccc3627a9c7da362fae2a56c2ca0
criptocore <coremanrique@gmail.com>
2024-10-09 23:47:49 -0500

    primer commit

---
+++ practicaTwo.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+   int numero_uno = 0, numero_dos = 0;
+
+   cout << "Ingrese el numero 1: ";
+   cin >> numero_uno;
+
+   cout << "Ingrese el numero 2: ";
+   cin >> numero_dos;
+
+   cout << "Suma: " << numero_uno + numero_dos << endl;
+   cout << "Resta: " << numero_uno - numero_dos << endl;
+   cout << "Producto: " << numero_uno * numero_dos << endl;
+   cout << "Divicion: " << numero_uno / numero_dos << endl;
+   cout << "Modulo: " << numero_uno % numero_dos << endl;
+
+
+   return 0;
+}
+
+
